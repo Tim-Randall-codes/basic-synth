291dcebe557bf5a4736139c03554456bfe7fad86
Tim Randall <trtimrandall@gmail.com>
2022-03-01 23:35:07 +1100

    new stuff

--- synthBasic/ContentView.swift
+++ synthBasic/ContentView.swift
@@ -10,44 +10,30 @@ import AudioKit
 import SoundpipeAudioKit
 
 let engine = AudioEngine()
-let osc = Oscillator(waveform: Table(.sine), frequency: 400, amplitude: 0.8)
-let filter = AmplitudeEnvelope(osc, attackDuration: 1, decayDuration: 0.01, sustainLevel: 0.8, releaseDuration: 0.9)
-// need to learn how to get the AmplitudeEnvelope to trigger release
+let osc = Oscillator(waveform: Table(.sine), frequency: 1600, amplitude: 0.8)
+let filter = AmplitudeEnvelope(osc, attackDuration: 1, decayDuration: 0.01, sustainLevel: 0.8, releaseDuration: 0.3)
+// can pass these as properties into the view structs!
 
 struct ContentView: View {
+    @State var started: Bool = false
     @State var playing: Bool = false
-
     var body: some View {
         VStack{
             Button(action:{
                 startItUp()
             }, label:{
-                Text("Start it up")
-            })
-            Button(action:{
-                startNStop()
-            }, label:{
-                Text("stop and play it")
+                Text("Play/stop")
             })
         }
     }
     func startItUp() {
-        osc.start()
-        engine.output = filter
-        do { try engine.start()}
-        catch { print("error starting engine")}
-        // stop
-        if playing == true {
-            filter.closeGate()
-            playing = false
-            }
-        // play
-        else {
-            filter.openGate()
-            playing = true
+        if started == false {
+            osc.start()
+            engine.output = filter
+            do { try engine.start()}
+            catch { print("error starting engine")}
+            started = true
         }
-    }
-    func startNStop() {
         // stop
         if playing == true {
             filter.closeGate()
