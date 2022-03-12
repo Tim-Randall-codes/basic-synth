ec2976f90ce7e3de39c690b499d21144526c6389
Tim Randall <trtimrandall@gmail.com>
2022-03-12 22:26:09 +1100

    has stuff i think is irrelevant, about to delete it

--- synthBasic/ContentView.swift
+++ synthBasic/ContentView.swift
@@ -37,22 +37,54 @@ struct Sound {
     lazy var filter = AmplitudeEnvelope(mixerTwo, attackDuration: a, decayDuration: d, sustainLevel: s, releaseDuration: r)
 }
 
-// view struct that has a button for each frequency. The 
-
 var soundOne = Sound(freq: 600, amp: 1, detune: 2, a: 0.3, d: 0.01, s: 0.8, r: 0.3)
 
+// the struct below will create a sound over the required frequencies.
+
+struct Sounds {
+    var a: Float
+    var d: Float
+    var s: Float
+    var r: Float
+    var detune: Float
+    var waveform: Table
+    lazy var osc1 = Oscillator(waveform: waveform, frequency: 440, amplitude: 0.8, detuningOffset: detune)
+    lazy var filter1 = AmplitudeEnvelope(osc1, attackDuration: a, decayDuration: d, sustainLevel: s, releaseDuration: r)
+    lazy var osc2 = Oscillator(waveform: waveform, frequency: 330, amplitude: 0.8, detuningOffset: detune)
+    lazy var filter2 = AmplitudeEnvelope(osc2, attackDuration: a, decayDuration: d, sustainLevel: s, releaseDuration: r)
+    lazy var osc3 = Oscillator(waveform: waveform, frequency: 220, amplitude: 0.8, detuningOffset: detune)
+    lazy var filter3 = AmplitudeEnvelope(osc3, attackDuration: a, decayDuration: d, sustainLevel: s, releaseDuration: r)
+    lazy var osc4 = Oscillator(waveform: waveform, frequency: 550, amplitude: 0.8, detuningOffset: detune)
+    lazy var filter4 = AmplitudeEnvelope(osc4, attackDuration: a, decayDuration: d, sustainLevel: s, releaseDuration: r)
+    lazy var osc5 = Oscillator(waveform: waveform, frequency: 660, amplitude: 0.8, detuningOffset: detune)
+    lazy var filter5 = AmplitudeEnvelope(osc5, attackDuration: a, decayDuration: d, sustainLevel: s, releaseDuration: r)
+}
+
+//the below array consists of instances of this sound with the sound's characteristics created. Names are commented
+
+//saw loud
+var tones : [Sounds] = [Sounds(a: 0.1, d: 0.01, s: 0.8, r: 0.3, detune: 1, waveform: Table(.sawtooth)),
+//square pluck
+                        Sounds(a: 0.1, d: 0.3, s: 0, r: 0.3, detune: 1, waveform: Table(.square))]
+
+
+// this is the view that shows all of the buttons. The sound the button plays is selected from the list using an integer inserted into this list.
+
 struct MainView: View {
+    var soundNumber: Int
     var body: some View {
         VStack{
-            ContentView(eng: engine, o1: osc, o2: osc2, m: mixer, f: filter)
-            ContentView(eng: engine2, o1: oscTwo, o2: osc2Two, m: mixerTwo, f: filterTwo)
-            ContentView(eng: soundOne.engine, o1: soundOne.osc, o2: soundOne.osc2, m: soundOne.mixer, f: soundOne.filter)
+            ButtonView2(o1: tones[soundNumber].osc1, f: tones[soundNumber].filter1)
+            ButtonView2(o1: tones[soundNumber].osc2, f: tones[soundNumber].filter2)
+            ButtonView2(o1: tones[soundNumber].osc3, f: tones[soundNumber].filter3)
+            ButtonView2(o1: tones[soundNumber].osc4, f: tones[soundNumber].filter4)
+            ButtonView2(o1: tones[soundNumber].osc5, f: tones[soundNumber].filter5)
         }
     }
 }
 
-struct ContentView: View {
-    var eng: AudioEngine
+struct ButtonView: View {
+    var eng = AudioEngine()
     var o1: Oscillator
     var o2: Oscillator
     var m: Mixer
@@ -88,8 +120,44 @@ struct ContentView: View {
     }
 }
 
+//use this button below
+
+struct ButtonView2: View {
+    var eng = AudioEngine()
+    var o1: Oscillator
+    var f: AmplitudeEnvelope
+    @State var started: Bool = false
+    @State var playing: Bool = false
+    var body: some View {
+            Button(action:{
+                startItUp()
+            }, label:{
+                Text("Play/stop")
+            })
+    }
+    func startItUp() {
+        if started == false {
+            o1.start()
+            eng.output = f
+            do { try eng.start()}
+            catch { print("error starting engine")}
+            started = true
+        }
+        // stop
+        if playing == true {
+            f.closeGate()
+            playing = false
+            }
+        // play
+        else {
+            f.openGate()
+            playing = true
+        }
+    }
+}
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView(eng: engine, o1: osc, o2: osc2, m: mixer, f: filter)
+        MainView(soundNumber: 1)
     }
 }
--- synthBasic/synthBasicApp.swift
+++ synthBasic/synthBasicApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct synthBasicApp: App {
     var body: some Scene {
         WindowGroup {
-            MainView()
+            MainView(soundNumber: 1)
         }
     }
 }
